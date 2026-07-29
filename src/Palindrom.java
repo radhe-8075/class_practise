ab1e7f9eef7befcd1c8f39d4cce4bc016e545bf6
Neeraj Baghel <tweensir9@gmail.com>
2026-07-29 13:54:53 +0530

    Initial commit

---
+++ src/Palindrom.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Palindrom {
+        static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int org=n;
+            int rev=0;
+            while(n>0){
+                int  ld=n%10;
+                rev=rev*10+ld;
+                n/=10;
+            }
+            if(rev==org)
+                System.out.println("Palindrome");
+            else System.out.println("Not palindrome");
+        }
+    }
+
---
+++ src/Reverse_Num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Reverse_Num {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int  ld=n%10;
+            rev=rev*10+ld;
+            n/=10;
+        }
+        System.out.println(rev);
+    }
+}
