07d70cbeccf319a701f1edbb70b371da5f283703
Neeraj Baghel <tweensir9@gmail.com>
2026-08-02 13:55:42 +0530

    normal even or odd

---
+++ src/Assignment1/Even_Odd.java
@@ -0,0 +1,18 @@
+package Assignment1;
+import java.util.Scanner;
+public class Even_Odd {;
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0 && n>0  ){
+            System.out.println("Even Number");
+        }
+        else if(n>=0){
+            System.out.println("Not s even number and odd number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+
+    }
+}
