e56fb0419c15d7896a7e6b3a1fc631b02e0c4405
Neeraj Baghel <tweensir9@gmail.com>
2026-08-02 18:45:06 +0530

    this is a computer way to easy and fast for even and odd in bit wise

--- src/Assignment1/Even_Odd.java
+++ src/Assignment1/Even_Odd.java
@@ -4,7 +4,7 @@ public class Even_Odd {;
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        if(n%2==0 && n>0  ){
+       /* if(n%2==0 && n>0  ){
             System.out.println("Even Number");
         }
         else if(n>=0){
@@ -12,7 +12,12 @@ public class Even_Odd {;
         }
         else{
             System.out.println("Odd Number");
-        }
+        } */
+        // second approch
+        if((n&1)==0) System.out.println("Even");
+        else System.out.println("odd");
+        sc.close();
+
 
     }
 }
