9d7149c6c8ae79b08b7f48af487cec37e86e5347
Neeraj Baghel <tweensir9@gmail.com>
2026-08-05 08:32:12 +0530

    depend on you which type of question you prefare

---
+++ src/_1d_Array/Array.java
@@ -0,0 +1,14 @@
+package _1d_Array;
+
+import java.util.Scanner;
+
+public class Array {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+       // int marks[]={50,99,98,60,40};
+       // System.out.println(Arrays.toString(marks));
+        int marks[]=new int[n];
+
+    }
+}
