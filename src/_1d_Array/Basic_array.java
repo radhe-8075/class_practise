851feeb4457ee9609e2c862f8ce222fdc2da26cf
Neeraj Baghel <tweensir9@gmail.com>
2026-08-05 14:05:42 +0530

    depend on you which type of question you prefare

---
+++ src/_1d_Array/Basic_array.java
@@ -0,0 +1,15 @@
+package _1d_Array;
+
+import java.util.Arrays;
+
+public class Basic_array {
+    static void main(String[] args) {
+        int n;
+        int []arr={5,78,96,45,96};
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+           // System.out.println(Arrays.toString(arr));
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
