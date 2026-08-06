a6b759ac328f0fc2c1c48eb8ddcdbdb23455bf57
Neeraj Baghel <tweensir9@gmail.com>
2026-08-06 08:54:54 +0530

    frist solution by while loop and second solution for loop 2

--- src/_1d_Array/Reverse_array.java
+++ src/_1d_Array/Reverse_array.java
@@ -2,15 +2,22 @@ package _1d_Array;
 
 public class Reverse_array {
     static void main(String[] args) {
-        int []nums={89,78,56,20};
-        int start=0;
-        int end=nums.length-1;
-        int tem=nums[0];
-        while(start<end){
+        int []nums={89,78,56,20,8};
+      // int start=0;
+       int end=nums.length-1;
+       // int tem=nums[0];
+       /* while(start<end){
             tem=nums[start];
             nums[start]=nums[end];
             nums[end]=tem;
             start++;
+            end--;
+        }*/
+        for(int i=0; i< nums.length/2; i++){
+           int tem=nums[i];
+            nums[i]=nums[end];
+            nums[end]=tem;
+
             end--;
         }
         for(int i=0; i< nums.length; i++){
