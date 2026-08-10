81ac53cc45a3a4836f90a3e3f0abb6e0d9afd793
Neeraj Baghel <tweensir9@gmail.com>
2026-08-10 12:37:55 +0530

    Reverse Right aligned star pattren by using one outer loop and two inner loops

---
+++ src/Pattern/Reverse_Aligned_Star.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Reverse_Aligned_Star {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j=n; j>=i; j--){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        sc.close();
+    }
+}
