384e3a0a8714c7f07b5321939faa2502d705249c
Neeraj Baghel <tweensir9@gmail.com>
2026-08-10 09:54:09 +0530

    hidden case

---
+++ src/Hidden_Case/_01.java
@@ -0,0 +1,18 @@
+package Hidden_Case;
+
+import java.util.Scanner;
+
+public class _01 {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int n= sc.nextInt();
+        sc.nextLine();
+        String city=sc.nextLine();
+
+       // System.out.println("name:"+name);
+     //   System.out.println("roll number:"+n);
+     //   System.out.println("city:"+city);
+        System.out.println(name+" "+n+" "+city);
+    }
+}
