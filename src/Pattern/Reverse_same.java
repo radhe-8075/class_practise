ded923007f9da84908b2ebc69e6206727f6e6222
Neeraj Baghel <tweensir9@gmail.com>
2026-08-10 12:57:25 +0530

    reverse same nubmer pattren by using one outer loop and two inner loops

---
+++ src/Pattern/Reverse_same.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Reverse_same {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=n; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
