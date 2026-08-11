6618a00cc5e7dfcf70f0c435dd55f04e8c0f3598
Neeraj Baghel <tweensir9@gmail.com>
2026-08-11 09:10:54 +0530

    BMI  by "%.2f",bmi

--- src/Assignment1/BMI.java
+++ src/Assignment1/BMI.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class BMI {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-
+        double w=sc.nextDouble();
+        float h=sc.nextFloat();
+        double bmi=w/(h*h);
+        System.out.printf("BMI: %.2f",bmi);
+        if(bmi<18.5) System.out.println(" Underweight ");
+        else if(bmi<25.0) System.out.println(" Normal weight ");
+        else if(bmi<30.0) System.out.println(" Overweight ");
+        else System.out.println(" Obese");
+        sc.close();
     }
 }
