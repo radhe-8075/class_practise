c1b00b6d2e579dc670243e5657d501787a49d02d
Neeraj Baghel <tweensir9@gmail.com>
2026-08-14 23:29:46 +0530

    Basic Input of Array

---
+++ src/Assignment2/Basic_Input.java
@@ -0,0 +1,13 @@
+package Assignment2;
+import java.util.*;
+public class Basic_Input {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
