e2b93e40457f5f125902567bb331e2e50272f960
Neeraj Baghel <tweensir9@gmail.com>
2026-09-06 23:48:45 +0530

    COUNT

---
+++ src/String/count.java
@@ -0,0 +1,13 @@
+package String;
+
+public class count {
+        public static void main(String[] args) {
+
+            String str = "Java is very easy";
+
+            String[] words = str.split(" ");
+
+            System.out.println("Words = " + words.length);
+        }
+    }
+
