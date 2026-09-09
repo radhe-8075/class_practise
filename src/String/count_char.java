57e66f93005933ffacdf3f71bb06f8ec3c82a054
Neeraj Baghel <tweensir9@gmail.com>
2026-09-09 16:39:42 +0530

    count char and index find

---
+++ src/String/count_char.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.Scanner;
+
+public class count_char {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+
+        int count=0;
+       /* for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            count++;
+        } */
+        System.out.println(count);
+    }
+}
