2ec214c50c63bb12b7698a74339b885acc56e276
Neeraj Baghel <tweensir9@gmail.com>
2026-09-09 16:39:17 +0530

    substring use

---
+++ src/String/substring.java
@@ -0,0 +1,11 @@
+package String;
+
+import java.util.Scanner;
+
+public class substring {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s="PROGRAMMING";
+        System.out.println(s.substring(3,7));
+    }
+}
