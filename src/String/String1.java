5d04419f3027992c4d533446d3c5dfbbcbc154d9
Neeraj Baghel <tweensir9@gmail.com>
2026-09-09 11:14:19 +0530

    string 1 this is my baisc consecpt start

---
+++ src/String/String1.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class String1 {
+    public static void main(String[]args){
+        String s=new String("Hello World");
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(s.length()-1));
+    }
+}
