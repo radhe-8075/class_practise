d6655c3214ff5c3fb027109dad78a7539b3e93d8
Neeraj Baghel <tweensir9@gmail.com>
2026-09-09 17:06:13 +0530

    contains

---
+++ src/String/contains.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Scanner;
+
+public class contains {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s="radhe baghel";
+        String f="ba";
+        System.out.println(s.contains(f));
+    }
+}
