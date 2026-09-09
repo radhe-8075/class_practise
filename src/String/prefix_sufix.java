c6ad6e63ce58fe20a48d2fc790a4a38f9854818b
Neeraj Baghel <tweensir9@gmail.com>
2026-09-09 17:05:56 +0530

    prefix

---
+++ src/String/prefix_sufix.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Scanner;
+
+public class prefix_sufix {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s="javaprogramming";
+        System.out.println(s.startsWith("java"));
+        System.out.println(s.endsWith("programming"));
+    }
+}
