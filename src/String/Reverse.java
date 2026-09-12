75a8af89cbfe6e9ba3357114f4124857678bbb0b
Neeraj Baghel <tweensir9@gmail.com>
2026-09-12 22:52:06 +0530

    reverse string in java

--- src/String/Reverse.java
+++ src/String/Reverse.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Reverse {
     static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s="   ";
-       // String s=sc.nextLine();
-        String rev="";
-        for(int i=s.length()-1;  i>=0;  i--){
-            rev+=s.charAt(i);
-        }
-        System.out.println(rev);
+        String s="COMPUTER";
+        char f=s.charAt(0);
+        char l=s.charAt(s.length()-1);
+
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(s.length()-1));
+        System.out.println(0);
+        System.out.println(s.length()-1);
+
+        System.out.println(s.indexOf(f));
+        System.out.println(s.lastIndexOf(l));
+
+
     }
 }
