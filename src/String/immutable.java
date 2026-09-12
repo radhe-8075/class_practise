1afe4af0da36acb4026c2854c35ef59170f30c62
Neeraj Baghel <tweensir9@gmail.com>
2026-09-12 22:51:03 +0530

    string immutable

---
+++ src/String/immutable.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class immutable {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "Java12345";
+        String f = "JAVa";
+        System.out.println(s+" "+"radhe");
+
+    }
+}
